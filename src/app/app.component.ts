2360ca28c671a4ebfb1860d5ab216c5c2a262e5b
Artie Fischer <artie@curetechnologies.us>
2021-04-18 09:55:13 -0600

    json checklist init
    to be made into checklist store that is toggleable off/on as a modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "RAF 2021";
+  checklist = {
+    app: {
+      checklistStore: {
+        "shows/hides": false,
+        updates: false,
+      },
+      toastsService: {
+        inits: false,
+        toasts: false,
+        service: false,
+        query: false,
+      },
+    },
+  };
 }
