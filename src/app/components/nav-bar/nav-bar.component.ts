d70cad3492ee12d2e82f2c2e9916ed98978e17f0
Artie Fischer <artie@curetechnologies.us>
2021-04-18 14:39:46 -0600

    wip: routing setup for navigation to component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ChecklistService } from "../../shared/checklist-container/state/checklist.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "raf-nav-bar",
@@ -7,9 +8,18 @@ import { ChecklistService } from "../../shared/checklist-container/state/checkli
   styleUrls: ["./nav-bar.component.scss"],
 })
 export class NavBarComponent implements OnInit {
-  constructor(private checklistService: ChecklistService) {}
+  constructor(
+    private checklistService: ChecklistService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   openChecklist = () => this.checklistService.open();
+
+  navTo(page: string) {
+    this.router
+      .navigate([`/${page}`])
+      .then((x) => console.log("Navigate to: ", page));
+  }
 }
