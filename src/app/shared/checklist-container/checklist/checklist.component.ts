5124df2e15c00a99487b3a7d806a8b5ba8d181c2
Artie Fischer <artie@curetechnologies.us>
2021-04-18 22:25:21 -0600

    wip: inits forms manager
    attempting to setup a form for the checklist components so that the form updates and then submits a whole new "checklist" upon store.update

--- src/app/shared/checklist-container/checklist/checklist.component.ts
+++ src/app/shared/checklist-container/checklist/checklist.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { ChecklistObject, ChecklistStatus } from "../state/checklist.model";
 import { ChecklistService } from "../state/checklist.service";
 import { ChecklistQuery } from "../state/checklist.query";
-import { map } from "rxjs/operators";
+import { NgFormsManager } from "@ngneat/forms-manager";
 
 @Component({
   selector: "raf-checklist",
@@ -14,9 +14,19 @@ export class ChecklistComponent implements OnInit {
   @Output() destroy: EventEmitter<any> = new EventEmitter<any>();
   constructor(
     private service: ChecklistService,
-    private query: ChecklistQuery
+    private query: ChecklistQuery,
+    private formsManager: NgFormsManager
   ) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    // ! make the form!
+    // go through each key title.action with the status as the val
+    // when the form updates - "formChanges" submit the whole thing back together as a new checklist...
+    // read from the form value in the list list container....
+  }
 
   updateChecklist(
     newList: ChecklistStatus[],
