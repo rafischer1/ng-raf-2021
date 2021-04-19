9e41e71616a538ce136c5a73f321af84a89eae11
Artie Fischer <artie@curetechnologies.us>
2021-04-19 12:19:09 -0600

    chore: adds highcharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
@@ -14,11 +13,11 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { MatIconModule } from "@angular/material/icon";
 import { MatOptionModule } from "@angular/material/core";
 import { ToastsWebComponent } from "./components/features/toasts-web/toasts-web.component";
-import { ToastButtonComponent } from './components/features/toasts-web/toast-button/toast-button.component';
-import { ToastsModalComponent } from './shared/toasts-container/toasts-modal/toasts-modal.component';
+import { ToastButtonComponent } from "./components/features/toasts-web/toast-button/toast-button.component";
+import { ToastsModalComponent } from "./shared/toasts-container/toasts-modal/toasts-modal.component";
 import { FormsModule } from "@angular/forms";
-import { ChecklistListComponent } from './shared/checklist-container/checklist/checklist-list/checklist-list.component';
-import { ChecklistItemComponent } from './shared/checklist-container/checklist/checklist-list/checklist-item/checklist-item.component';
+import { ChecklistListComponent } from "./shared/checklist-container/checklist/checklist-list/checklist-list.component";
+import { ChecklistItemComponent } from "./shared/checklist-container/checklist/checklist-list/checklist-item/checklist-item.component";
 
 @NgModule({
   declarations: [
@@ -42,7 +41,7 @@ import { ChecklistItemComponent } from './shared/checklist-container/checklist/c
     BrowserAnimationsModule,
     MatIconModule,
     MatOptionModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
