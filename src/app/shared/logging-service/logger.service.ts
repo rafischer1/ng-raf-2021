33cbd01ba68dd6fc3a2138ca279b429cb898c665
Artie Fischer <artie@curetechnologies.us>
2021-04-21 22:45:16 -0600

    fix: adds comments

--- src/app/shared/logging-service/logger.service.ts
+++ src/app/shared/logging-service/logger.service.ts
@@ -8,23 +8,31 @@ import { Log } from "./log.interface";
 export class LoggerService {
   constructor(private store: LoggerStore) {}
 
+  // * toggle store's active state
   toggleStore = () =>
     this.store.update((_old) => {
-      return { active: !_old.active, logs: [] };
+      return { active: !_old.active };
     });
 
+  // * adds a log to store log[]
   addLog = (log: Log) => {
     setTimeout(() => {
-      const clg = `(${log.context.toUpperCase()}): ${log.text}`;
-      console.log("%c " + clg, "color: limegreen; font-family: monospace;");
+      this.logToConsole(log);
       this.store.update((_old) => {
         return { logs: [log, ..._old.logs] };
       });
     }, 500);
   };
 
+  // * clear all logs without hiding container
   clearLogs = () =>
     this.store.update((_) => {
       return { logs: [] };
     });
+
+  // * duplicate log in console
+  logToConsole = (log: Log) => {
+    const clg = `(${log.context.toUpperCase()}): ${log.text}`;
+    console.log("%c " + clg, "color: limegreen; font-family: monospace;");
+  };
 }
--- src/app/shared/logging-service/logger/logger.component.spec.ts
+++ src/app/shared/logging-service/logger/logger.component.spec.ts
@@ -1,25 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LoggerComponent } from './logger.component';
+import { LoggerComponent } from "./logger.component";
+import { createComponentFactory, Spectator } from "@ngneat/spectator";
+import { of } from "rxjs";
 
-describe('LoggerComponent', () => {
-  let component: LoggerComponent;
-  let fixture: ComponentFixture<LoggerComponent>;
+describe("LoggerComponent", () => {
+  let spectator: Spectator<LoggerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoggerComponent ]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory({
+    component: LoggerComponent,
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoggerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should create", () => {
+    expect(spectator.component).toBeTruthy();
   });
 });
