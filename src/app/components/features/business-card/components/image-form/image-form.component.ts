264eb975428cd5b022a0b51fe28ce68fa0053bfa
Artie Fischer <artie@curetechnologies.us>
2021-04-28 14:50:51 -0600

    wip: image added and randomized

--- src/app/components/features/business-card/components/image-form/image-form.component.ts
+++ src/app/components/features/business-card/components/image-form/image-form.component.ts
@@ -2,43 +2,45 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import * as faker from "faker";
 import { BusinessCardImage } from "../../state/business-card.model";
 import { BusinessCardQuery } from "../../state/business-card.query";
-import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
-import { map } from "rxjs/operators";
 
-@UntilDestroy()
 @Component({
   selector: "raf-image-form",
   templateUrl: "./image-form.component.html",
   styleUrls: ["./image-form.component.scss"],
 })
 export class ImageFormComponent implements OnInit {
-  selectedWidth: number = 55;
-  selectedHeight: number = 55;
+  currentImage: BusinessCardImage = {
+    width: 60,
+    height: 60,
+    opacity: 1,
+    active: false,
+    borderRadius: 2,
+    src: "",
+  };
 
   @Output()
   imageAdd: EventEmitter<BusinessCardImage> = new EventEmitter<BusinessCardImage>();
 
   @Output()
   toggleImage: EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor(private query: BusinessCardQuery) {}
+  constructor() {}
+
+  ngOnInit(): void {
+    this.currentImage.src = faker.image.abstract();
+  }
 
   addImage = () => {
-    this.imageAdd.emit({
-      width: this.selectedWidth,
-      height: this.selectedHeight,
-      src: faker.image.abstract(),
-      opacity: 1,
-      active: true,
-      borderRadius: 2,
-    });
+    this.imageAdd.emit({ ...this.currentImage, active: true });
   };
 
   randomImage = () =>
-    faker.image.abstract(this.selectedWidth, this.selectedHeight);
+    this.imageAdd.emit({
+      ...this.currentImage,
+      src: faker.image.image(),
+      active: true,
+    });
 
   toggle = () => {
     this.toggleImage.emit(false);
   };
-
-  ngOnInit(): void {}
 }
