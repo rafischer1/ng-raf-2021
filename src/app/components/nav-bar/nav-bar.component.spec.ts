51d12b6e362d2a442b171892ce21efeddce53bba
Artie Fischer <artie@curetechnologies.us>
2021-05-03 15:49:38 -0600

    feat: testing translation in nav bar component spec

--- src/app/components/nav-bar/nav-bar.component.spec.ts
+++ src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,12 +1,24 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { createComponentFactory, Spectator } from "@ngneat/spectator/jest";
 import { NavBarComponent } from "./nav-bar.component";
+import { getTranslocoModule } from "../../transloco/transloco-testing.module";
+import { TranslocoService } from "@ngneat/transloco";
+import { LanguageToggleComponent } from "../../shared/language-toggle/language-toggle.component";
+import { LanguageIconComponent } from "../../shared/language-toggle/language-icon/language-icon.component";
+import { mockProvider } from "@ngneat/spectator";
+import { Router } from "@angular/router";
 
 describe("NavBarComponent", () => {
   let spectator: Spectator<NavBarComponent>;
 
   const createComponent = createComponentFactory({
     component: NavBarComponent,
+    declarations: [LanguageToggleComponent, LanguageIconComponent],
+    providers: [mockProvider(Router)],
+    imports: [
+      getTranslocoModule({
+        translocoConfig: { reRenderOnLangChange: true },
+      }),
+    ],
   });
 
   beforeEach(() => {
@@ -16,5 +28,13 @@ describe("NavBarComponent", () => {
 
   it("should create", () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  it("should translate", () => {
+    expect(spectator.query("button#toasts")).toHaveText("En.nav.toasts");
+    const service = spectator.inject(TranslocoService);
+    service.setActiveLang("pt");
+    spectator.detectChanges();
+    expect(spectator.query("button#toasts")).toHaveText("Pt.nav.toasts");
   });
 });
