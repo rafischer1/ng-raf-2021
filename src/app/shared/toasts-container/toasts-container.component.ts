bd7f2c4e907e8a7fa223649110bb1b76a7e2ccad
Robert Arthur Fischer (Artie) <39342327+rafischer1@users.noreply.github.com>
2022-03-15 22:21:50 -0600

    refactor: initialize observable and refactor component cycle

--- src/app/shared/toasts-container/toasts-container.component.ts
+++ src/app/shared/toasts-container/toasts-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Observable } from "rxjs";
 import { Toast, ToastsService } from "./state/toasts.service";
 import { ToastsQuery } from "./state/toasts.query";
@@ -8,14 +8,10 @@ import { ToastsQuery } from "./state/toasts.query";
   templateUrl: "./toasts-container.component.html",
   styleUrls: ["./toasts-container.component.scss"],
 })
-export class ToastsContainerComponent implements OnInit {
-  toasts$: Observable<Toast[]>;
+export class ToastsContainerComponent {
+  toasts$ = this.query.select((state) => state.toasts).pipe();
 
   constructor(private service: ToastsService, private query: ToastsQuery) {}
-
-  ngOnInit() {
-    this.toasts$ = this.query.select((state) => state.toasts).pipe();
-  }
 
   deleteToast = (i: number) => this.service.deleteToast(i);
 }
