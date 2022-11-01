72b016acf95dd9f97913c73115317902b825dd4a
rafischer1 <artiefischer@gmail.com>
2022-11-01 19:42:18 -0400

    feat: spookyname input service + update

---
+++ src/app/components/features/Halloween/state/halloween.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { HalloweenStore } from "./halloween.store";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HalloweenService {
+  constructor(private store: HalloweenStore) {}
+
+  updateSpookyName = (name: string) => {
+    this.store.update((_old) => {
+      return { ..._old, spookyName: name };
+    });
+  };
+}
