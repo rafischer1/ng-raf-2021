0a77b4779eded56c8e827d685846bf56396ed8b3
rafischer1 <artiefischer@gmail.com>
2023-01-31 09:44:15 -0500

    injection token for toast store

--- src/app/shared/toasts-container/state/toasts.service.ts
+++ src/app/shared/toasts-container/state/toasts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { ToastsStore } from "./toasts.store";
 
 export interface Toast {
@@ -11,20 +11,19 @@ export interface Toast {
   providedIn: "root",
 })
 export class ToastsService implements Toast {
+  private _store = inject(ToastsStore);
   message = "";
   type = "";
   link = "";
 
-  constructor(private store: ToastsStore) {}
-
   showToast = (type: string, message: string, link?: string) => {
-    this.store.update((old) => {
+    this._store.update((old) => {
       return { toasts: [...old.toasts, { type, message, link }] };
     });
   };
 
   deleteToast = (i: number) =>
-    this.store.update((_old) => {
+    this._store.update((_old) => {
       const old = [..._old.toasts];
       old.splice(i, 1);
       return { toasts: old };
