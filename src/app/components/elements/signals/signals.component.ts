ed94e437f26a1d3f09695fef0ed6962915a99190
rafischer1 <artiefischer@gmail.com>
2023-06-01 10:27:14 -0400

    signals setup

--- src/app/components/elements/signals/signals.component.ts
+++ src/app/components/elements/signals/signals.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: "signals-element",
-  template: `<div>Signals Element {{ message }}</div>`,
+  template: `<div>Signals Element:::: {{ count() }}</div>
+    <br />
+    <div>{{ setTime() }}</div>`,
   styles: [],
 })
 export class SignalsComponent {
-  @Input() message!: string;
+  count = signal(0);
+  setTime = ():void => {
+    setTimeout(() => {
+      this.count.set(3);
+    }, 5000);
+  };
 }
